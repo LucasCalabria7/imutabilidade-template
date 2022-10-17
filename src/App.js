595360e4a4d8709bc9d5a0acb5db30dbe9239b65
Lucas Calabria <lucascalabria86@gmail.com>
2022-10-17 11:27:47 -0300

    React UseState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Garagem from "./Componentes/Garagem";
 import { GlobalStyled } from "./globalStyled";
+import {useState} from 'react'
 
 export default function App() {
   const nome = "Turma";
+  const [nomeEstado, setNomeEstado] = useState("Lucas Calabria")
   function apresentaGaragem() {
     alert(`Boas vindas à garagem de ${nome}`);
+  }
+
+  const mudarNome = () => {
+    setNomeEstado("React Learner!")
   }
 
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome={nome} mensagemAprentacao={apresentaGaragem} />
+      <Garagem nome={nomeEstado} mensagemAprentacao={mudarNome} />
     </div>
   );
 }
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,30 +1,68 @@
 import Carro from "./Carro";
 import { Button, EstilizacaoDaGaragem } from "./styledGaragem";
+import {useState} from 'react'
 
 function Garagem(props) {
+
+  const [automovel, setAutomovel] = useState ({
+    cor: "Violeta",
+    ano: 2030,
+    flex: "ye"
+  })
+
+  const [automovel2, setAutomovel2] = useState ({
+    cor: "Laranja",
+    ano: 2002,
+    flex: "nop"
+  })
+
+  const [automovel3, setAutomovel3] = useState ({
+    cor: "Marrom",
+    ano: 1987,
+    flex: "Quase"
+  })
+
+  const [automovel4, setAutomovel4] = useState ({
+    cor: "Bege",
+    ano: 2022,
+    flex: "siuu"
+  })
+
+  const trocaCarro = () => {
+    setAutomovel({cor: "UltraVioleta", ano: 2025, flex: "Logic"})
+    setAutomovel2({cor: "Vinho", ano: 2000, flex: "Try again"})
+    setAutomovel3({cor: "Aqua", ano: 1990, flex: "Jamais"})
+    setAutomovel4({cor: "Preto Fosco", ano: 2022, flex: "Agora sim!"})
+  };
+
   return (
     <EstilizacaoDaGaragem>
-      <h1>Garagem da {props.nome}</h1>
+      <h1>Garagem do {props.nome}</h1>
       <Button onClick={props.mensagemAprentacao}>Mensagem</Button>
       <Carro
-        adicionadoPor={props.nome}
-        cor={"Vermelho"}
-        ano={2022}
-        flex={"true"}
+        adicionadoPor= {props.nome}
+        cor= {automovel.cor}
+        ano= {automovel.ano}
+        flex= {automovel.flex}
       />
       <Carro
         adicionadoPor={props.nome}
-        cor={"Verde"}
-        ano={2020}
-        flex={"false"}
+        cor={automovel2.cor}
+        ano={automovel2.ano}
+        flex={automovel2.flex}
       />
       <Carro
         adicionadoPor={props.nome}
-        cor={"Amarelo"}
-        ano={2009}
-        flex={"false"}
+        cor={automovel3.cor}
+        ano={automovel3.ano}
+        flex={automovel3.flex}
       />
-      <Carro adicionadoPor={props.nome} cor={"Rosa"} ano={2022} flex={"true"} />
+      <Carro adicionadoPor={props.nome} 
+        cor={automovel4.cor} 
+        ano={automovel4.ano} 
+        flex={automovel4.flex} />
+
+        <Button onClick={trocaCarro} >Mudar Nomes</Button>
     </EstilizacaoDaGaragem>
   );
 }
